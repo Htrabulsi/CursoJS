480b58aa282806a391dbc0ec5d17517d592052a9
Htrabulsi <halainrachid@hotmail.com>
2025-12-10 14:38:05 -0300

    ex013

--- aula11/ex009.js
+++ aula11/ex009.js
@@ -1,7 +1,7 @@
 var país = 'Brasil'
-console.log(`Vivendo em ${país}`)
+console.log(`Vivendo em ${país}`)  
 if (país != 'Brasil') {
     console.log('Você é estrangeiro!')
-} else {
+} else {                                    // Condição composta!
     console.log('Você é brasileiro!')
 }   
