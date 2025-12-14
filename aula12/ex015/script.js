905c6a4b77e4efe12c46ed22a6e52b45abefed3c
Htrabulsi <halainrachid@hotmail.com>
2025-12-14 17:22:03 -0300

    final ex015

--- aula12/ex015/script.js
+++ aula12/ex015/script.js
@@ -5,7 +5,8 @@ function verificar() {
     var res = document.getElementById('res')
     if (fano.value.length == 0 || Number(fano.value) > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
-    } else {
+    } 
+    else {
         var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var gênero = ''
@@ -15,35 +16,45 @@ function verificar() {
             gênero = 'Homem'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', 'bebe-m.png')
+                img.setAttribute('src', 'foto-bebe-m.png')
             } else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', 'jovem-m.png')
+                img.setAttribute('src', 'foto-jovem-m.png')
             } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', 'adulto-m.png')
+                img.setAttribute('src', 'foto-adulto-m.png')
             } else {
                 //Idoso
-                img.setAttribute('src', 'idoso-m.png')
+                img.setAttribute('src', 'foto-idoso-m.png')
             }
-        } else if (fsex[1].checked) {
+        } 
+        else if (fsex[1].checked) {
             gênero = 'Mulher'
             if (idade >= 0 && idade < 10) {
                 //Criança
-                img.setAttribute('src', 'bebe-f.png')
+                img.setAttribute('src', 'foto-bebe-f.png')
             } else if (idade < 21) {
                 //Jovem
-                img.setAttribute('src', 'jovem-f.png')
+                img.setAttribute('src', 'foto-jovem-f.png')
             } else if (idade < 50) {
                 //Adulto
-                img.setAttribute('src', 'adulto-f.png')
+                img.setAttribute('src', 'foto-adulto-f.png')
             } else {
                 //Idoso
-                img.setAttribute('src', 'idoso-f.png')
+                img.setAttribute('src', 'foto-idoso-f.png')
             }
         }
+
         res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+// Estilizando a imagem via JavaScript - fora da proposta do curso em vídeo
+        img.style.borderRadius = '50%'
+        img.style.objectFit = 'cover' // opcional, melhora o corte
+        img.style.marginTop = '20px'
+        img.style.width = '300px'
+        img.style.height = '300px' // se quiser garantir formato circular
+
         res.appendChild(img)
+
     }
 }   
