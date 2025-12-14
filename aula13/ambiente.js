3f4469c437c36716f8ee8d7fe04d0ff6028f5a31
Htrabulsi <halainrachid@hotmail.com>
2025-12-14 17:51:25 -0300

    aula13

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+
+
+// Estrutura de repetição em loop usando while, com teste logico no início.
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`Passos ${c}`)  
+    c++
+}  
+*/
+
+// Estrutura de repetição em loop usando do-while. com teste lógico no final.
+
+var d = 1
+do {
+    console.log(`Passos ${d}`)
+    d++
+} while (d <= 6)    
